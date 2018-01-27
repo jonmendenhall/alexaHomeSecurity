6c11f0fee8789eb759918f8abc10b8263732ef2f
Jon Mendenhall <jonmend1028@gmail.com>
2018-01-27 09:19:43 -0500

    1

--- base.py
+++ base.py
@@ -14,7 +14,7 @@ radio.begin(0, 17)
 
 radio.setPayloadSize(32)
 radio.setChannel(0x60) 
-radio.setDataRate(NRF24.BR_1MBPS)
+radio.setDataRate(NRF24.BR_2MBPS)
 radio.setPALevel(NRF24.PA_MIN)
 
 radio.setAutoAck(True)
